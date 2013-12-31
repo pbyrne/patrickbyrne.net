0290421791ec5556d859c2669addeec9d5265e5a
Patrick Byrne <code@patrickbyrne.net>
2013-12-31 14:20:28 -0600

    Move config to be more production-ready

--- config.rb
+++ config.rb
@@ -1,47 +1,22 @@
 require "slim"
 
-###
-# Compass
-###
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy pages (http://middlemanapp.com/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
-
-###
-# Helpers
-###
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
+# CONFIGURATION
+set :css_dir, 'stylesheets'
+set :images_dir, 'images'
 # Reload the browser automatically whenever files change
 activate :livereload, host: "localhost"
-
 # Make pretty URLs for every page (e.g. /foo rather than /foo.html)
 activate :directory_indexes
+
+# BUILD-SPECIFIC CONFIGURATION
+configure :build do
+  # For example, change the Compass output style for deployment
+  activate :minify_css
+
+  # Enable cache buster
+  activate :asset_hash
+  activate :cache_buster
+end
 
 # Methods defined in the helpers block are available in templates
 # helpers do
@@ -50,26 +25,4 @@ activate :directory_indexes
 #   end
 # end
 
-set :css_dir, 'stylesheets'
 
-set :js_dir, 'javascripts'
-
-set :images_dir, 'images'
-
-# Build-specific configuration
-configure :build do
-  # For example, change the Compass output style for deployment
-  # activate :minify_css
-
-  # Minify Javascript on build
-  # activate :minify_javascript
-
-  # Enable cache buster
-  # activate :asset_hash
-
-  # Use relative URLs
-  # activate :relative_assets
-
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
-end
