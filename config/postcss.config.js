fbfdfe703f5833b1cbac6fa422caf7dbb12c252d
Patrick Byrne <34053+pbyrne@users.noreply.github.com>
2022-06-11 22:29:08 -0500

    Remove usage of nested CSS

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   plugins: [
-    require('tailwindcss/nesting')(require('postcss-nesting')),
     require("tailwindcss")("./config/tailwind.config.js"),
     require("autoprefixer"),
   ],
