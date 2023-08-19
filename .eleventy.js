76ea0480020b47331d23959e9eecbdb5f9c06a93
Patrick Byrne <34053+pbyrne@users.noreply.github.com>
2023-08-18 22:22:32 -0500

    Switch to Vite for asset processing (#139)
    Because:
    
    * It gives us asset fingerprinting out of the box, so we don't rely on
      Netlify's now-deprecated bundling.
    * It gives really good presets out of the box, generally.
    * It integrates directly with eleventy-dev-server.
    
    Solution:
    
    * Remove PostCSS manual configuration
    * Remove autoprefixer
    * Pull Vite in and plug into Eleventy
    * Remove now-unnecessary npm-run-all which ran the manual Eleventy and
      PostCSS commands for build and local serving.

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,11 @@
+const EleventyVitePlugin = require("@11ty/eleventy-plugin-vite");
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./source/images")
   eleventyConfig.addPassthroughCopy("./source/javascripts")
   eleventyConfig.addPassthroughCopy("./source/stylesheets")
+
+  eleventyConfig.addPlugin(EleventyVitePlugin);
 
   return {
     dir: { input: "source", output: "_site" }
