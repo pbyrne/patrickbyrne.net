7de419fc794dae2c6ed3ef2fd7189d0e0c405a00
Patrick Byrne <34053+pbyrne@users.noreply.github.com>
2024-10-27 14:05:32 -0500

    fixup! Bump @11ty/eleventy from 2.0.1 to 3.0.0

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,11 +1,13 @@
-const EleventyVitePlugin = require("@11ty/eleventy-plugin-vite");
+import EleventyVitePlugin from "@11ty/eleventy-plugin-vite";
+import UpgradeHelper from "@11ty/eleventy-upgrade-help";
 
-module.exports = function(eleventyConfig) {
+export default async function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./source/images")
   eleventyConfig.addPassthroughCopy("./source/javascripts")
   eleventyConfig.addPassthroughCopy("./source/stylesheets")
 
   eleventyConfig.addPlugin(EleventyVitePlugin);
+  eleventyConfig.addPlugin(UpgradeHelper);
 
   return {
     dir: { input: "source", output: "_site" }
